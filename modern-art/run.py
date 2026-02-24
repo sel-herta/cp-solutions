3d139d10efce5dbf6bb33a627ee9d9b6aa8948c7
sel-herta <yazssuo@gmail.com>
2026-02-24 11:50:44 -0800

    code for modern art

--- modern-art/run.py
+++ modern-art/run.py
@@ -7,6 +7,26 @@ def solve():
     rows, cols = int(input()), int(input())
     moves = int(input())
     
+    # if painted number is even, its black.
+    # if painted number is odd, its gold.
+    
+    # so basically, if row and col are differing (odd vs even) its gonna be gold
+    
+    rows_color = [0 for _ in range(rows)]
+    cols_color = [0 for _ in range(cols)]
+    
+    gold = 0
+    for _ in range(moves):
+        op1, op2 = input().split()
+        op2 = int(op2) - 1
+        
+        if op1 == 'R':
+            rows_color[op2] += 1
+        else:
+            cols_color[op2] += 1
+    
+    
+    
 
 if __name__ == "__main__":
     solve()
