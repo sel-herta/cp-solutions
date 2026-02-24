1d429dfe78bd971ec862332d734f2d5b6b047b1c
William Tu <tuwi@oregonstate.edu>
2026-02-24 15:09:16 -0800

    start sushi problem

---
+++ atcoder-sushi/README.md
@@ -0,0 +1,9 @@
+# Educational DP Contest AtCoder J - Sushi
+
+**[Problem Link](https://dmoj.ca/problem/dpj)**
+
+---
+
+### Intuition
+
+fff
---
+++ atcoder-sushi/run.py
@@ -0,0 +1,15 @@
+import sys
+from collections import defaultdict, deque
+
+sys.setrecursionlimit(200000)
+
+def solve():
+    N = int(input())
+    plates = list(map(int, input().split()))
+    
+            
+            
+    
+
+if __name__ == "__main__":
+    solve()
