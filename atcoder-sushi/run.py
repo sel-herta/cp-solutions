bc5ef9f6e8b7ac78a3205cbd7b71c0ec2aad7c6f
William Tu <tuwi@oregonstate.edu>
2026-02-24 15:12:17 -0800

    comments on sushi problem

--- atcoder-sushi/run.py
+++ atcoder-sushi/run.py
@@ -7,6 +7,15 @@ def solve():
     N = int(input())
     plates = list(map(int, input().split()))
     
+    # we can roll a N sided die, call the result i
+    # the result from this die is the plate we look at
+    # if plates[i] > 0, then eat from it, decrement plates[i] by 1
+    # do nothing otherwise
+    # at any point, we HAVE to roll a die
+    # how could we make this into a dp problem? what are the states
+    # this is a probability problem atp
+    # i guess we can track how many plates we have finished or smthn
+    # 
             
             
     
