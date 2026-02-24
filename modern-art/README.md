7cbcbbc9c916fd7a0730ab637716d9d84b36b9b7
sel-herta <yazssuo@gmail.com>
2026-02-24 01:10:29 -0800

    start modern art problem

---
+++ modern-art/README.md
@@ -0,0 +1,3 @@
+# CCC '21 S2 - Modern Art
+
+**[Problem Link](https://dmoj.ca/problem/ccc21s2)**
---
+++ modern-art/run.py
@@ -0,0 +1,12 @@
+import sys
+from collections import defaultdict, deque
+
+sys.setrecursionlimit(200000)
+
+def solve():
+    rows, cols = int(input()), int(input())
+    moves = int(input())
+    
+
+if __name__ == "__main__":
+    solve()
