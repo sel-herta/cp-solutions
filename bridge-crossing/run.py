d4c261edbd758d50d6b9c85f07157e09985356be
sel-herta <yazssuo@gmail.com>
2026-02-26 01:29:37 -0800

    still need to construct the list of people that crossed

--- bridge-crossing/run.py
+++ bridge-crossing/run.py
@@ -7,30 +7,53 @@ def solve():
     M = int(input())
     Q = int(input())
     people = []
+    times = []
     for _ in range(Q):
         name = input()
         time = int(input())
-        people.append([name, time])
-    # we can either
-    # add person to group
-    # make curr group cross bridge
-    memo = {}
-    def dfs(i, curr_group, person):
-        if i >= len(people) or len(curr_group) > M:
-            return float('inf')
-        state = (i, curr_group)
-        if state in memo:
-            return memo[state]
-        add_person = float('inf')
-        leave = float('inf')
-        add_person = dfs(i + 1, curr_group.append([people[i]]))
-        if len(curr_group) >= 1:
-            leave = max(curr_group) + dfs(i, [])
-        res = min(add_person, leave)
-        memo[state] = res
-        return res
-
+        people.append(name)
+        times.append(time)
     
+    # could either make the current group leave
+    # or add to the queue
+    # so its like a for loop, from i up to i + M
+    # dp lol
+    
+    leaving = []
+    
+    memo = {}
+    def dfs(i):
+        nonlocal leaving
+        if i >= len(people):
+            return 0
+        if i in memo:
+            return memo[i]
+        # make the singular person leave (always valid)
+        initial_leave = times[i] + dfs(i + 1)
+        p1 = people[i]
+        # make groups up to M leave from i
+        group_leave = float('inf')
+        j = i
+        max_leaver = times[i]
+        for k in range(1, M):
+            if i + k < len(people):
+                max_leaver = max(max_leaver, times[i + k])
+                new = max_leaver + dfs(i + k + 1)
+                if new <= group_leave:
+                    group_leave = new
+                    j = i + k + 1
+            else:
+                break
+        if initial_leave < group_leave:
+            leaving.append(p1)
+        else:
+            leaving.extend([people[i:j]])
+        res = min(group_leave, initial_leave)
+        memo[i] = res
+        return res
+    print(dfs(0))
+    print(leaving)
+                
         
 
 if __name__ == "__main__":
